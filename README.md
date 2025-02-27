95d1bd6afc5d320703a898af81d010fa7aeb7a98
Stefan Iacob <s.t.iacob@gmail.com>
2025-02-27 14:21:24 +0100

    restructure files + joni's code + test code

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This repository contains the code, data and analysis discussed in the distance-b
 paper [Exploiting Signal Propagation Delays to Match Task Memory Requirements in Reservoir Computing](https://doi.org/10.3390/biomimetics9060355).
 
 Analysis for the paper [Memory–Non-Linearity Trade-Off in Distance-Based Delay Networks](https://www.mdpi.com/2313-7673/9/12/755) 
-is based on the same evolved networks, and can be found in [IPC-Analysis.ipynb](IPC-Analysis.ipynb). Contents of 
+is based on the same evolved networks, and can be found in [IPC-Analysis.ipynb](analysis-notebooks/IPC-Analysis.ipynb). Contents of 
 this notebook are still being cleaned up and annotated.
 
 Analysis for the initial [2022 paper on DDNs](https://link.springer.com/chapter/10.1007/978-3-031-21753-1_21) is shown 
@@ -20,19 +20,19 @@ Code for CMA-ES hyperparameter optimisation is included for two benchmark tasks
 Optimizing DDNs or baseline ESNs for NARMA tasks can be done by running [NARMA_experiment.py](NARMA_experiment.py) with
 the appropriate parameters. Results from previous optimization runs can be found
 in [NARMA-10_results_23](results/NARMA-10_results_23) and [NARMA-30_results_23](results/NARMA-30_results_23), also used in
-the [analysis notebook](MC-Analysis.ipynb) to generate paper figures.
+the [analysis notebook](analysis-notebooks/MC-Analysis.ipynb) to generate paper figures.
 
 ### Mackey-Glass
 Optimizing DDNs, ADDNs, adaptive ESNs or baseline ESNs for Mackey-Glass tasks can be done by
 running [mg_experiment.py](mg_experiment.py) with the appropriate parameters. Results from previous optimization runs
 can be found in [ADDN_further_experiments](results/ADDN_further_experiments), also used in
-the [analysis notebook](MC-Analysis.ipynb) to generate paper figures.
+the [analysis notebook](analysis-notebooks/MC-Analysis.ipynb) to generate paper figures.
 
 ## Testing
 Example code for how to test best optimized networks shown in [testOptimized.py](examples/testOptimized.py)
 
 ## Paper Figures and data analysis
-All paper figures can be reproduced using the included [jupyter notebook](MC-Analysis.ipynb).
+All paper figures can be reproduced using the included [jupyter notebook](analysis-notebooks/MC-Analysis.ipynb).
 
 ## Visual Example
 See [visual_example.py]([examples/visual_example.py]) for a DDN or ESN (either random or optimized) simulated with a
@@ -42,7 +42,7 @@ GUI, visually showing differences in network responses to various inputs.
 Information processing capacity (IPC) is used in 
 [Exploiting Signal Propagation Delays to Match Task Memory Requirements in Reservoir Computing](https://doi.org/10.3390/biomimetics9060355).
 To understand how this is computed, and how to use the [capacities tools](Capacities/capacities.py), have a look at 
-[capacitiesExample.ipynb](examples/capacitiesExample.ipynb).
+[capacitiesExample.ipynb](examples/capacitiesDDNExample.ipynb).
 
 Code for computing IPC can be found in [Capacities](Capacities) folder, written by prof. dr. Joni Dambre, published in 
 [Dambre, Joni, David Verstraeten, Benjamin Schrauwen, and Serge Massar. “Information Processing Capacity of Dynamical Systems.” Scientific Reports 2, no. 1 (July 19, 2012): 514.
--- examples/testOptimized.py
+++ examples/testOptimized.py
@@ -1,70 +1,101 @@
 import numpy as np
 from network import tanh_activation
 import pickle as pkl
-from utils import createNARMA30, eval_candidate_lag_gridsearch_NARMA
+from utils import createNARMA10, createNARMA30, eval_candidate_lag_gridsearch_NARMA
 from datetime import date
 import argparse
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Experiment configuration",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-n10", "--narma_10", action="store_true", type=bool)
-    parser.add_argument("-n30", "--narma_30", action="store_true", type=bool)
-
-    args = parser.parse_args()
-    config = vars(args)
-    delay = config['delay']
-    experiment_data = {
+EXPERIMENT_DATA = {
         'NARMA-30': {
             'DDN': {
-                'path': "NARMA-30_results_23/NARMA30_old_ddn_results_n101_k4_date_2023-12-11.p"
+                'path': "../results/NARMA-30_results_23/NARMA30_old_ddn_results_n101_k4_date_2023-12-11.p"
             },
             'ESN': {
-                'path': "NARMA-30_results_23/NARMA30_old_bl_results_n101_k4_date_2024-03-18.p"
+                'path': "../results/NARMA-30_results_23/NARMA30_old_bl_results_n101_k4_date_2024-03-18.p"
             }
         },
         'NARMA-10': {
             'ESN': {
-                'path': "NARMA-10_results_23/NARMA10_old_bl_results_n51_k4_date_2024-03-18.p"
+                'path': "../results/NARMA-10_results_23/NARMA10_old_bl_results_n51_k4_date_2024-03-18.p"
             },
             'DDN': {
-                'path': "NARMA-10_results_23/NARMA10_old_ddn_results_n51_k4_date_2023-12-09.p"
+                'path': "../results/NARMA-10_results_23/NARMA10_old_ddn_results_n51_k4_date_2023-12-09.p"
             }
         }
     }
 
-    np.random.seed(42)
-    data_train_42 = np.array(createNARMA30(20000)).reshape((2, 20000))
-    data_test_42 = np.array(createNARMA30(10000)).reshape((2, 10000))
-    np.random.seed()
+def load_data(data_dict):
+    for net_type in data_dict:
+        path = data_dict[net_type]['path']
+        with open(path, 'rb') as f:
+            data = pkl.load(f)
+            data_dict[net_type]['es_dict'] = data
 
-    network_resamples = 40
 
-    test_results = {
-        '1K': {
-            'DDN': [],
-            'ESN': []
-        },
-        '4K': {
-            'DDN': [],
-            'ESN': []
+def test_optimized(data_dict, resamples, data_train, data_test):
+    results = {}
+    for net_type in data_dict:
+        print('Network type: {}'.format(net_type))
+        print('Testing candidate for {} resamples...'.format(resamples))
+        es_dict = data_dict[net_type]['es_dict']
+        net = es_dict['example net']
+        best_p = es_dict['evolutionary strategy'].best.x
+        warmup = 400
+        alphas = [10e-7, 10e-5, 10e-3]
+        results[net_type] = []
+        for resample in range(resamples):
+            print('resample ' + str(resample) + '...')
+            best_net = net.get_new_network_from_serialized(best_p)
+            print('training readout and testing network...')
+            t, v, m = eval_candidate_lag_gridsearch_NARMA(best_net, data_train, data_test, warmup=warmup, alphas=alphas)
+            results[net_type].append(v)
+            print('score: ' + str(np.min(v)))
+    return results
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Experiment configuration",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-n10", "--narma_10", action='store_true', help='Test N-10 experiment')
+    parser.add_argument("-n30", "--narma_30", action='store_true', help='Test N-30 experiment')
+    parser.add_argument("-r", "--repetitions", action='store', type=int, help='Nr of resamples per candidate')
+
+    args = parser.parse_args()
+
+    config = vars(args)
+    test_n10 = config['narma_10']
+    test_n30 = config['narma_30']
+    network_resamples = config['repetitions']
+    tasks = []
+    if test_n10:
+        np.random.seed(42)
+        task_data = {
+            'name': 'NARMA-10',
+            'train': np.array(createNARMA10(20000)).reshape((2, 20000)),
+            'test': np.array(createNARMA10(10000)).reshape((2, 10000))
         }
-    }
+        np.random.seed()
+        tasks.append(task_data)
+    if test_n30:
+        np.random.seed(42)
+        task_data = {
+            'name': 'NARMA-30',
+            'train': np.array(createNARMA30(20000)).reshape((2, 20000)),
+            'test': np.array(createNARMA30(10000)).reshape((2, 10000))
+        }
+        np.random.seed()
+        tasks.append(task_data)
+    if len(tasks) == 0:
+        print('Neither N-10 nor N-30 selected, please add either -n10, -n30 or both flags to test any networks.')
 
-    # for k_size in experiment_data:
-    #     for net_type in experiment_data[k_size]:
-    #         print('Condition: ' + str(k_size) + ', ' + str(net_type))
-    #         net = experiment_data[k_size][net_type]['example_net']
-    #         best_p = experiment_data[k_size][net_type]['es_dict']['evolutionary strategy'].best.x
-    #         for resample in range(network_resamples):
-    #             print('resample ' + str(resample) + '...')
-    #             best_net = net.get_new_network_from_serialized(best_p)
-    #             print('training readout and testing network...')
-    #             t, v, m = eval_candidate_lag_gridsearch(best_net, data_train_42, data_test_42, warmup=400,
-    #                                                     alphas=alphas)
-    #             test_results[k_size][net_type].append(v)
-    #             print('score: ' + str(np.min(v)))
-    # filename = 'test_' +  str(date.today()) + '.p'
-    # save_file = 'results-2022-paper/test_results/' + filename
-    # with open(save_file, 'wb') as f:
-    #     pkl.dump(test_results, f)
+    for task in tasks:
+        task_name = task['name']
+        data_train = task['train']
+        data_test = task['test']
+        print('Testing optimized ' + task_name + ' networks from ' + str(EXPERIMENT_DATA[task_name]))
+        load_data(EXPERIMENT_DATA[task_name])
+        task_results = test_optimized(EXPERIMENT_DATA[task_name], network_resamples, data_train, data_test)
+        filename = 'test_' + task_name + '_' +  str(date.today()) + '.p'
+        save_path = '../results/' + task_name + '_results_23/' + filename
+        print('Testing ' + task_name + ' finished, saving test results as ' + save_path)
+        with open(save_path, 'wb') as f:
+            pkl.dump(task_results, f)
